195856f8525f5fa4a373f5263c25e6da80b2064d
Reshoo-18 <reshooranjan7@gmail.com>
2026-05-24 22:45:46 +0530

    tavily integration for webSearch

--- INVOKE_LLM/app.js
+++ INVOKE_LLM/app.js
@@ -1,23 +1,96 @@
-import  Groq  from "groq-sdk";
+import Groq from "groq-sdk";
+import {tavily } from '@tavily/core'
 
-const groq=new Groq({apiKey:process.env.GROQ_API_KEY})
+const tvly=tavily({apiKey:process.env.TAVILY_API_KEY})
 
-async function main(){
-   const Completion=await groq.chat.completions.create({
-    model :'llama-3.3-70b-versatile',
-    temperature:0,
-    messages:[
-        {
-            role:'system',
-            content:'You are a personal assistance who answer the asked question'
-        },
-        {
-            role:'user',
-            content:'when i phone 16 launched?'
-        },
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+async function main() {
+  const completions = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    temperature: 0,
+
+    messages: [
+      {
+        role: "system",
+        content: `You are a personal assistance who answer the asked question.
+            you have access to following tools
+            1. webSearch({query} : {query:string}) //Search the latest information and realtime data o the internet`,
+      },
+      {
+        role: "user",
+        content: "when i phone 16 launched?",
+      },
     ],
-   })
-   console.log(Completion.choices[0].message.content)
+
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "webSearch",
+          description:
+            "Search the latest information and realtime data o the internet",
+
+          parameters: {
+            type: "object",
+
+            properties: {
+              query: {
+                type: "string",
+                description: "The search query to perform serch on",
+              },
+            },
+
+            required: ["query"],
+          },
+        },
+      },
+    ],
+
+    tool_choice: "auto",
+  });
+
+  console.log(
+    JSON.stringify(
+      completions.choices[0].message.content,
+      null,
+      2
+    )
+  );
+
+  const toolCalls = completions.choices[0].message.tool_calls;
+
+  if (!toolCalls) {
+    console.log(
+      `Assistance ${completions.choices[0].message.content}`
+    );
+    return;
+  }
+
+  for (const tool of toolCalls) {
+    console.log("tool", tool);
+
+    const functionName = tool.function.name;
+
+    const functionParams = JSON.parse(
+      tool.function.arguments
+    );
+
+    if (functionName === "webSearch") {
+      const toolResult = await webSearch(functionParams);
+
+      console.log("tool result", toolResult);
+    }
+  }
 }
 
-main()
+main();
+
+async function webSearch({ query }) {
+  console.log("result is comming")
+  const response= await tvly.search(query)
+  console.log('Response: ', response)
+  return "i phone 16 was launched on 20 september 2024";
+}
