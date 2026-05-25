aa542f7006608da1233ee621081bdd3a3ec4ab4f
Reshoo-18 <reshooranjan7@gmail.com>
2026-05-25 23:48:27 +0530

    structue output

--- INVOKE_LLM/app.js
+++ INVOKE_LLM/app.js
@@ -1,96 +1,25 @@
 import Groq from "groq-sdk";
-import {tavily } from '@tavily/core'
+import dotenv from "dotenv"
 
-const tvly=tavily({apiKey:process.env.TAVILY_API_KEY})
+dotenv.config()
+const groq=new Groq({apiKey:process.env.GROQ_API_KEY})
 
-const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY,
-});
-
-async function main() {
-  const completions = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-
-    messages: [
-      {
-        role: "system",
-        content: `You are a personal assistance who answer the asked question.
-            you have access to following tools
-            1. webSearch({query} : {query:string}) //Search the latest information and realtime data o the internet`,
-      },
-      {
-        role: "user",
-        content: "when i phone 16 launched?",
-      },
-    ],
-
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description:
-            "Search the latest information and realtime data o the internet",
-
-          parameters: {
-            type: "object",
-
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform serch on",
-              },
-            },
-
-            required: ["query"],
+async function main(){
+    
+      const completion =await groq.chat.completions.create({
+        model:'llama-3.3-70b-versatile',
+        messages:[
+          {
+            role:'system',
+            content:'Hy i am a assistant '
           },
-        },
-      },
-    ],
-
-    tool_choice: "auto",
-  });
-
-  console.log(
-    JSON.stringify(
-      completions.choices[0].message.content,
-      null,
-      2
-    )
-  );
-
-  const toolCalls = completions.choices[0].message.tool_calls;
-
-  if (!toolCalls) {
-    console.log(
-      `Assistance ${completions.choices[0].message.content}`
-    );
-    return;
-  }
-
-  for (const tool of toolCalls) {
-    console.log("tool", tool);
-
-    const functionName = tool.function.name;
-
-    const functionParams = JSON.parse(
-      tool.function.arguments
-    );
-
-    if (functionName === "webSearch") {
-      const toolResult = await webSearch(functionParams);
-
-      console.log("tool result", toolResult);
-    }
-  }
+          {
+            role :'user',
+            content:'who are you?'
+          }
+        ]
+       });
+       console.log(completion.choices[0].message.content)
 }
 
-main();
-
-async function webSearch({ query }) {
-  console.log("result is comming")
-  const response= await tvly.search(query)
-  console.log('Response: ', response)
-  return "i phone 16 was launched on 20 september 2024";
-}
+main()
