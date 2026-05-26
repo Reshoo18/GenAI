877d2b5dbb1e34f2861ab581379daaf367d4a92f
Reshoo-18 <reshooranjan7@gmail.com>
2026-05-26 23:46:45 +0530

    promt engineering

--- INVOKE_LLM/app.js
+++ INVOKE_LLM/app.js
@@ -7,19 +7,71 @@ const groq=new Groq({apiKey:process.env.GROQ_API_KEY})
 async function main(){
     
       const completion =await groq.chat.completions.create({
+        //response_format:{type:'json_object'},
         model:'llama-3.3-70b-versatile',
+        
         messages:[
           {
             role:'system',
-            content:'Hy i am a assistant '
+            content:`you are the smart assistance who answer the asked question
+            you have access to following tools:
+           You can use webSearch tool to search latest information.`
           },
           {
             role :'user',
-            content:'who are you?'
+            content:'when i phone 16 launched?'
           }
-        ]
+        ],
+        tools:[
+          {
+      "type": "function",
+      "function": {
+        "name": "webSearch",
+        "description": "Search the latest information and realtime data on internet",
+        "parameters": {
+          // JSON Schema object
+          "type": "object",
+          "properties": {
+            "query": {
+              "type": "string",
+              "description": "the search query to perform on search"
+            },
+           
+          },
+          "required": ["query"]
+        }
+      }
+    }
+        ],
+        tool_choice:'auto',
        });
-       console.log(completion.choices[0].message.content)
+
+       const toolCalls=completion.choices[0].message.tool_calls
+
+        if(!toolCalls){
+          console.log(`AI ${completion.choices[0].message.content}`)
+          return
+        }
+
+        for(const tool of toolCalls){
+          console.log('tool',tool)
+          const functionName=tool.function.name;
+          const functionParams=tool.function.arguments;
+          if(functionName==='webSearch'){
+           const toolresult= await webSearch(JSON.parse(functionParams))
+           console.log('toolResult:',toolresult)
+          }
+        }
+       //console.log(JSON.stringify(completion.choices[0].message,null,2))
 }
 
-main()
+main();
+
+
+
+function webSearch({query}){
+
+  console.log('calling webSearch......')
+
+  return "i phone 16 was launched on september 2024";
+}
