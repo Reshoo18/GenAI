c4dec4cfbd56f0f272e6ae6744491cde509f1a08
Reshoo-18 <reshooranjan7@gmail.com>
2026-05-28 23:11:22 +0530

    LLM Complete

--- INVOKE_LLM/app.js
+++ INVOKE_LLM/app.js
@@ -1,27 +1,43 @@
+import readline from 'node:readline/promises'
 import Groq from "groq-sdk";
 import {tavily} from "@tavily/core"
 import dotenv from "dotenv"
+import { stdin, stdout } from 'node:process';
 
 dotenv.config()
 const tvly= tavily({apiKey:process.env.TAVILY_API_KEY})
 const groq=new Groq({apiKey:process.env.GROQ_API_KEY})
 
 async function main(){
+      
+      const rl=readline.createInterface({input:process.stdin,output:process.stdout})
+      
       const messages=[
           {
             role:'system',
-            content:`you are the smart assistance who answer the asked question
-            you have access to following tools:
-           You can use webSearch tool to search latest information.`
+          content:'You are a smart assistant.'
           },
-          {
-            role :'user',
-            content:'when i phone 16 launched?'
-          }
+          // {
+          //   role :'user',
+          //   content:'when i phone 16 launched?'
+          // }
         ]
-      const completions =await groq.chat.completions.create({
+
+        while(true){
+          const question= await rl.question('you: ')
+           if(question==='bye'){
+            break;
+           }
+          messages.push({
+            role:'user',
+            content:question
+          })
+
+          while (true){
+           const completions =await groq.chat.completions.create({
         //response_format:{type:'json_object'},
-        model:'llama-3.3-70b-versatile',
+        temperature:0,
+        model:'openai/gpt-oss-120b',
         messages:messages,
         
         tools:[
@@ -45,29 +61,23 @@ async function main(){
       }
     }
         ],
-        tool_choice: {
-  type: "function",
-  function: {
-    name: "webSearch"
-  }
-
-},
+        
        });
       messages.push(completions.choices[0].message)
        const toolCalls=completions.choices[0].message.tool_calls
 
         if(!toolCalls){
           console.log(`AI ${completions.choices[0].message.content}`)
-          return
+          break;
         }
 
         for(const tool of toolCalls){
-          console.log('tool',tool)
+         // console.log('tool',tool)
           const functionName=tool.function.name;
           const functionParams=tool.function.arguments;
           if(functionName==='webSearch'){
            const toolresult= await webSearch(JSON.parse(functionParams))
-           console.log('toolResult:',toolresult)
+           //console.log('toolResult:',toolresult)
            messages.push({
             tool_call_id:tool.id,
             role:'tool',
@@ -76,41 +86,8 @@ async function main(){
            })
           }
         }
-            const completions2 =await groq.chat.completions.create({
-        //response_format:{type:'json_object'},
-        model:'llama-3.3-70b-versatile',
-        
-        messages:messages,
-        tools:[
-          {
-      "type": "function",
-      "function": {
-        "name": "webSearch",
-        "description": "Search the latest information and realtime data on internet",
-        "parameters": {
-          // JSON Schema object
-          "type": "object",
-          "properties": {
-            "query": {
-              "type": "string",
-              "description": "the search query to perform on search"
-            },
-           
-          },
-          "required": ["query"]
         }
-      }
-    }
-        ],
-        tool_choice: {
-  type: "function",
-  function: {
-    name: "webSearch"
-  }
-  
-},
-       });
-       console.log(JSON.stringify(completions2.choices[0].message,null,2))
+        }      
 }
 
 main();
@@ -121,9 +98,11 @@ async function webSearch({query}){
 
   console.log('calling webSearch......')
         const respose= await tvly.search(query)
-        console.log('Response:',respose)
+       // console.log('Response:',respose)
 
         const finalResult=respose.results.map((result)=>result.content).join("\n");;
+
+        
         
   return finalResult;
 }
