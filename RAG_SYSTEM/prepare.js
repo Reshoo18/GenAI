9f46415721ae55165f4d486d572422e32a6a8d89
Reshoo-18 <reshooranjan7@gmail.com>
2026-06-10 14:02:23 +0530

    vector embeddings

--- RAG_SYSTEM/prepare.js
+++ RAG_SYSTEM/prepare.js
@@ -1,15 +1,20 @@
 import {PDFLoader} from '@langchain/community/document_loaders/fs/pdf'
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
 
 export async function indexTheDocument(filePath){
-  console.log("File Path:", filePath);
-
-  const loader = new PDFLoader(filePath);
+ 
+  const loader = new PDFLoader(filePath,{splitPages:false});
 
   const docs = await loader.load();
-
-  console.log("Documents Loaded:");
-  console.log(docs);
-
-  return docs;
+  
+  
+  const textsplitters= new RecursiveCharacterTextSplitter({
+    chunkSize:500,
+    chunkOverlap:100,
+  })
+  
+  const text= await textsplitters.splitText(docs[0].pageContent)
+  console.log(text.length);
+  
 }
